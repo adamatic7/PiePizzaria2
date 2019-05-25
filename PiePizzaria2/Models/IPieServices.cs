86338fc4ad19bf569739ee31d5eab6b38185f73c
Adam Miranda <adamballimiranda@mail.weber.edu>
2019-05-25 16:58:12 -0600

    Add project files.

---
+++ PiePizzaria2/Models/IPieServices.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PiePizzaria2.Models
+{
+    public interface IPieServices
+    {
+
+        IEnumerable<Pie> GetAllPies();
+
+        Pie GetPieById(int pieId);
+    }
+}
